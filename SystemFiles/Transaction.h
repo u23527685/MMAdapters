aecb651f0ef7bbf92431275f4504ac9d3d886724
JerushaThaver <thaverjerusha19@gmail.com>
2025-10-14 15:29:27 +0200

    Transaction

---
+++ SystemFiles/Transaction.h
@@ -0,0 +1,28 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include <string>
+#include "TransactionSnapshot.h"
+#include "PaymentStrategy.h"
+
+class Transaction {
+private:
+    std::string customerName;
+    std::string plantName;
+    double amount;
+    int quantity;
+    PaymentStrategy *paymentMethod;
+
+public:
+    Transaction(const std::string &customerName, const std::string &plantName, double amount, int quantity);
+    void setTransaction(const std::string &customerName, const std::string &plantName, double amount, int quantity);
+
+    std::string getDetails();
+    TransactionSnapshot createSnapshot();
+    void restoreSnapshot(const TransactionSnapshot &snapshot);
+
+    void setPaymentMethod(PaymentStrategy *strategy);
+    void processPayment();
+};
+
+#endif
