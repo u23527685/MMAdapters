c9d6c62354cee4371014192e16d4178d17d71f87
JerushaThaver <thaverjerusha19@gmail.com>
2025-10-22 20:46:45 +0200

    Update EFTPaymentStrategy.cpp

--- SystemFiles/EFTPaymentStrategy.cpp
+++ SystemFiles/EFTPaymentStrategy.cpp
@@ -4,7 +4,7 @@
 EFTPaymentStrategy::EFTPaymentStrategy(const std::string& accountNumber) : accountNumber(accountNumber) {}
 
 void EFTPaymentStrategy::pay(double amount, const std::string& customerName) {
-    std::cout << "Processing EFT payment of $" << amount 
+    std::cout << "Processing EFT payment of R" << amount 
               << " for " << customerName << " using account number: " 
               << accountNumber << std::endl;
 }
