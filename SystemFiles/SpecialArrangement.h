55c96df2e1e7f1aedb8581e8977eee0fc596a90f
JerushaThaver <thaverjerusha19@gmail.com>
2025-10-22 12:25:45 +0200

    Decorator

---
+++ SystemFiles/SpecialArrangement.h
@@ -0,0 +1,13 @@
+#ifndef SPECIAL_ARRANGEMENT_H
+#define SPECIAL_ARRANGEMENT_H
+
+#include "PlantDecorator.h"
+
+class SpecialArrangement : public PlantDecorator {
+public:
+    SpecialArrangement(Plant* plant);
+    std::string getDescription() const override;
+    double getPrice() const override;
+};
+
+#endif // SPECIAL_ARRANGEMENT_H
