4e57afbf9c7c4946c54325f3c9fe86e8baa36a50
JerushaThaver <thaverjerusha19@gmail.com>
2025-10-28 11:14:39 +0200

    removed file

--- SystemFiles/TransactionHistory.cpp
+++ SystemFiles/TransactionHistory.cpp
@@ -10,7 +10,8 @@ TransactionSnapshot TransactionHistory::getSnapshot(int index) {
         return history[index];
     }
     throw std::out_of_range("Snapshot index out of range");
+}
 
-int TransactionHistory::getHistorySize(){
+int TransactionHistory::getHistorySize() const {
     return static_cast<int>(history.size());
 }
