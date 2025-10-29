7c67aaa58d0db9c990518243312e840547b0881c
rachel-m-kambala <rachel.m.kambala@gmail.com>
2025-10-29 12:20:29 +0200

    Command and Observer code removed from C&S

--- SystemFiles/AddStock.cpp
+++ SystemFiles/AddStock.cpp
@@ -1,9 +1,1 @@
-#include "AddStock.h"
 
-AddStock::AddStock(PlantStock* s, std::string type, int quantity)
-    : stock(s), plantType(type), quantity(quantity) {}
-
-void AddStock::execute() {
-    if (stock)
-        stock->addStock(plantType, quantity);
-}
--- SystemFiles/InventoryObserver.cpp
+++
@@ -1,3 +0,0 @@
-#include "InventoryObserver.h"
-
-InventoryObserver::~InventoryObserver() {}
