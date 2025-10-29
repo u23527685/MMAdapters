46dcd3b64619a03941912b088f58e821b684b2d7
rachel-m-kambala <rachel.m.kambala@gmail.com>
2025-10-29 12:02:34 +0200

    Customer implemented

---
+++ SystemFiles/Customer.h
@@ -0,0 +1,27 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+#include <vector>
+#include "Order.h"
+#include "Staff.h"
+
+class Customer {
+private:
+    std::string name;
+    std::vector<Order> orders; 
+
+public:
+    Customer();
+    explicit Customer(const std::string& customerName);
+
+    std::string getName() const;
+    void setName(const std::string& customerName);
+
+    void placeOrder(const Order& order);
+    void viewOrders() const;
+
+    void makeQuery(Staff& staff, const std::string& queryMessage) const;
+};
+
+#endif
