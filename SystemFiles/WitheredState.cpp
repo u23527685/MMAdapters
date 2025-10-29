e7dd314a972fd732d8bb54e3f74203cb83cc0ef5
cobusbotha <u23556502@tuks.co.za>
2025-10-29 12:33:40 +0200

    Slightly more detailed Withered State output

--- SystemFiles/WitheredState.cpp
+++ SystemFiles/WitheredState.cpp
@@ -6,7 +6,7 @@
 #include "PlantCareRoutine.h"
 
 void WitheredState::applyCare(PlantLifeCycle* context, Plant* plant, PlantCareRoutine* routine) {
-    std::cout << "[WitheredState] Plant is withered. Care may be attempted, but recovery is unlikely." << std::endl;
+    std::cout << "[Withered State] " << plant->getName() << " is withered. Care may be attempted, but recovery is unlikely." << std::endl;
 
     if (plant->getCurrentWater() < 10) routine->Watering(plant);
     if (plant->getCurrentSunlight() < 10) routine->Sunlight(plant);
