e652896679074832479d8e42a57a4e30d7109296
u23527685 <u23527685@tuks.co.za>
2025-11-01 09:50:34 +0200

    fix: Builder added

--- SystemFiles/Builder.h
+++ SystemFiles/Builder.h
@@ -8,9 +8,9 @@ class Query;  // Forward declaration - don't include Query.h here
 class Builder{
     friend class Query;  // Use 'class' keyword
     protected:
-        Plant* item;
-        std::string type;
-        std::string question;
+        Plant* item=nullptr;
+        std::string type="";
+        std::string question="";
     public:
         virtual void setType(std::string type)=0;
         virtual void setQuestion(std::string question)=0;
