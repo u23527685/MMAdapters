7c1f3b499a4c4efb0953b80d64688c1849fda583
cobusbotha <u23556502@tuks.co.za>
2025-11-01 09:07:45 +0200

    Implemented a proxy

---
+++ SystemFiles/InventoryProxy.cpp
@@ -0,0 +1,21 @@
+#include "InventoryProxy.h"
+
+InventoryProxy::InventoryProxy(SalesFloorObserver* observer) : inventory(PlantInventory::getInstance()), salesObserver(observer) {}
+
+void InventoryProxy::buyPlant(Plant* plant, int quantity) {
+    std::string desc = plant ? plant->getDescription() : "(unknown)";
+    bool result = inventory->removeStock(plant, quantity);
+    if (result) {
+        std::cout << "Customer bought " << quantity << " " << desc << (quantity == 1 ? " plant." : " plants.") << std::endl;
+    } else {
+        std::cout << "Purchase failed: Not enough stock or invalid request." << std::endl;
+    }
+}
+
+void InventoryProxy::addStock(Plant* plant, int quantity) {
+    std::cout << "Exception: Access denied: Customers cannot add stock." << std::endl;
+}
+
+void InventoryProxy::removeStock(Plant*, int) {
+    std::cout << "Exception: Access denied: Customers cannot remove stock directly." << std::endl;
+}
