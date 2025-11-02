619d75ddbd47f9fca8391ba28c2682b86b5a6d87
TK <dubamthokozisi@gmail.com>
2025-11-02 13:50:24 +0200

    Added dexygen for greenhouse class

--- SystemFiles/Oak.h
+++ SystemFiles/Oak.h
@@ -2,8 +2,20 @@
 #define OAK_H
 #include "Plant.h"
 
+/**
+ * @class Oak
+ * @brief Represents an Oak tree plant
+ * 
+ * This class is a concrete implementation of the Plant class,
+ * representing an Oak tree in the plant inventory system.
+ */
 class Oak : public Plant {
     public:
+    /**
+     * @brief Constructs an Oak plant object
+     * @param price The price of the oak tree
+     * @param description A description of the oak tree
+     */
     Oak(double price, std::string description) : Plant(price, description) {}
 };
 
