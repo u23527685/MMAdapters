9a5a301d913e2145eaf5a215a40ef10cec7a30ed
JerushaThaver <thaverjerusha19@gmail.com>
2025-11-04 03:12:04 +0200

    Update BasePlant.h

--- SystemFiles/BasePlant.h
+++ SystemFiles/BasePlant.h
@@ -3,13 +3,15 @@
 #include <string>
 #include "Plant.h"
 
-class BasePlant : public Plant {
-public:
-  BasePlant();
-  BasePlant(double p, std::string desc);
-  virtual ~BasePlant() {}
-  std::string getDescription() const override;
-  double getPrice() const override;
+class BasePlant: public Plant {
+    public:
+    BasePlant();
+    BasePlant(double p, std::string desc);
+    virtual ~BasePlant() {}
+    std::string getDescription() const override;
+    double getPrice() const override;
+
+    
 };
 
 #endif
